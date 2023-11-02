b38ac109e4ca06442f1e49baae6a4f8f5a361f6a
Mitchell <55370047+mitchellonii@users.noreply.github.com>
2023-11-02 13:12:17 +1030

    a

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,18 +11,30 @@ import { useState, useEffect } from "react"
 
 function Return() {
     var [scrollAmount, setScrollAmount] = useState(0)
+    function isScrolledIntoView(el) {
+        var rect = el.getBoundingClientRect();
+        var elemTop = rect.top;
+        var elemBottom = rect.bottom;
+        var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+        return isVisible;
+    }
     function handleScroll(e) {
         setScrollAmount(e.target.scrollTop)
+        Array.from(document.querySelectorAll("#fadeOnScrollView")).forEach(e => {
+            if (isScrolledIntoView(e)) e.classList.add("fadeIn")
+            else e.classList.remove("fadeIn")
+        })
     }
     useEffect(() => {
         document.querySelector("#root").onscroll = handleScroll
     }, [])
+
     return <>   <div className="content">
         <div className="section">
             <img src={icon} alt="" className="iconAnimation" />
             <p className="iconSubtext">Auralis</p>
             <p className="sectionHeader">Music that makes you</p>
-            <div class="divlowerhalf">
+            <div className="divlowerhalf">
                 <div className="verticalScroll" style={{ transform: `translateX(-${scrollAmount / 3}px)` }}>
                     <div className="phoneDisplay" style={{ transform: `translateY(calc(50px - ${Math.pow(Math.sin(scrollAmount / 360), 2) * 50}px))` }}>
                         <img alt="" src={phone}></img>
@@ -57,10 +69,41 @@ function Return() {
                 </div>
             </div>
         </div>
-        <div className="section">
+        <div className="section" style={{ zIndex: 1 }}>
             <div className="gradientBackground"></div>
-            <div style={{ zIndex: 100, position: "absolute", opacity: Math.max(0, (scrollAmount - 200) / 400) > 1 ? 1 : Math.max(0, (scrollAmount - 200) / 400) }}>
-                <p>lorem ipsum</p>
+            <div className="centerText" style={{ zIndex: 100, position: "absolute", opacity: Math.max(0, (scrollAmount - 200) / 400) > 1 ? 1 : Math.max(0, (scrollAmount - 200) / 400) }}>
+                <br></br><br></br><p className="sectionHeaderCompact">Why Auralis?</p>
+                <br></br>
+                <p className={`sectionText bulitin`} id="fadeOnScrollView"><mark>100 billion</mark> songs at your fingertip</p>
+                <br></br>
+                <p className={`sectionText bulitin`} id="fadeOnScrollView">AI generated playlists based on <mark>your intrests</mark></p>
+                <br></br>
+                <p className={`sectionText bulitin`} id="fadeOnScrollView"><mark>Studio</mark> quality, <mark>uncompressed</mark> sound</p>
+                <br></br>
+                <p className={`sectionText bulitin`} id="fadeOnScrollView">Listen on any device, even when <mark>offline</mark></p>
+
+            </div>
+        </div>
+        <div className="section smallSection">
+            <div className="flexGrid">
+                <div>
+                    <p>With new accounts</p>
+                    <p>You could get 6 months free</p>
+                    <p>lorem ipsum</p>
+                    <button></button>
+                </div>
+                <div>
+                    <p>View all our</p>
+                    <p>Flexibile plans for any price range</p>
+                    <p>lorem ipsum</p>
+                    <button></button>
+                </div>
+                <div>
+                    <p>View all our</p>
+                    <p>Flexibile plans for any price range</p>
+                    <p>lorem ipsum</p>
+                    <button></button>
+                </div>
             </div>
         </div>
     </div></>
